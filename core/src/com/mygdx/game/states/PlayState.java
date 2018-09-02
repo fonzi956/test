ae17c36118fe0bb516eeb90d302d7251cd13ed38
fonzi956 <alfonsolozoya@ymail.com>
2018-09-02 07:02:48 -0500

    17th commit for test app done right spot

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -24,7 +24,12 @@ public class PlayState extends State {
     private Buildings mb1;
     private Buildings mb2;
     private Buildings mb3;
-    private Buildings mb4;
+    private Buildings sb1;
+    private Buildings sb2;
+    private Buildings bb;
+    private Buildings cl;
+    private Buildings tr;
+    private Buildings trd;
     private Texture bg;
     private String va;
     private String mn;
@@ -47,15 +52,20 @@ public class PlayState extends State {
 
         va = "VALID UNTIL";
         ti = mn + " 2:59 AM";
-        mb1 = new Buildings(0, 330, "bmb.png");
-        mb2 = new Buildings(0, 329, "sbg.png");
-        mb3 = new Buildings(0, 333, "blueb.png");
-        mb4 = new Buildings(0, 411, "clouds.png");
+        mb1 = new Buildings(0, 330, "tower.png");
+        mb2 = new Buildings(400, 330, "redthing.png");
+        mb3 = new Buildings(750, 330, "alamo.png");
+        tr = new Buildings(0, 329, "trees.png");
+        trd = new Buildings(0, 329, "treesD.png");
+        sb1 = new Buildings(-100, 329, "hotelbackbuilding.png");
+        sb2 = new Buildings(500, 329, "twobackbuildings.png");
+        bb = new Buildings(0, 333, "blueb.png");
+        cl = new Buildings(0, 411, "clouds.png");
         bird = new Bird(0, 324);
         //cam.setToOrtho(false, MyGdxGame.WIDTH, MyGdxGame.HEIGHT);
         //zooms in to the bird
         //cam.setToOrtho(false, MyGdxGame.WIDTH / 2, MyGdxGame.HEIGHT / 2);
-        bg = new Texture("Orbackground.png");
+        bg = new Texture("tworiders.png");//Orbackground
         batch = new SpriteBatch();
         font = new BitmapFont(Gdx.files.internal("myfont.fnt"));
         bfont = new BitmapFont(Gdx.files.internal("myBlodfont.fnt"));
@@ -98,7 +108,12 @@ public class PlayState extends State {
         mb1.update(dt);
         mb2.update(dt);
         mb3.update(dt);
-        mb4.update(dt);
+        tr.update(dt);
+        trd.update(dt);
+        sb1.update(dt);
+        sb2.update(dt);
+        bb.update(dt);
+        cl.update(dt);
         bird.update(dt);
         //cam.position.x = bird.getPosition().x + 80;
 
@@ -122,8 +137,28 @@ public class PlayState extends State {
             mb3.reposition(mb3.getPosition().x + 1500, mb3.getPosition().y);
         }
 
-        if(cam.position.x - (cam.viewportWidth / 2) > mb4.getPosition().x + mb4.getTexture().getWidth()){
-            mb4.reposition(mb4.getPosition().x + 1500, mb4.getPosition().y);
+        if(cam.position.x - (cam.viewportWidth / 2) > tr.getPosition().x + tr.getTexture().getWidth()){
+            tr.reposition(tr.getPosition().x + 1500, tr.getPosition().y);
+        }
+
+        if(cam.position.x - (cam.viewportWidth / 2) > tr.getPosition().x + trd.getTexture().getWidth()){
+            trd.reposition(trd.getPosition().x + 1500, trd.getPosition().y);
+        }
+
+        if(cam.position.x - (cam.viewportWidth / 2) > sb1.getPosition().x + sb1.getTexture().getWidth()){
+            sb1.reposition(sb1.getPosition().x + 1600, sb1.getPosition().y);
+        }
+
+        if(cam.position.x - (cam.viewportWidth / 2) > sb2.getPosition().x + sb2.getTexture().getWidth()){
+            sb2.reposition(sb2.getPosition().x + 1600, sb2.getPosition().y);
+        }
+
+        if(cam.position.x - (cam.viewportWidth / 2) > bb.getPosition().x + bb.getTexture().getWidth()){
+            bb.reposition(bb.getPosition().x + 1500, bb.getPosition().y);
+        }
+
+        if(cam.position.x - (cam.viewportWidth / 2) > cl.getPosition().x + cl.getTexture().getWidth()){
+            cl.reposition(cl.getPosition().x + 1500, cl.getPosition().y);
         }
 
 //        if(cam.position.x - (cam.viewportWidth / 2) > bird.getPosition().x + bird.getTexture().getWidth()){
@@ -143,30 +178,41 @@ public class PlayState extends State {
 
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bg, 0,0);
+
 //        for (Tube tube : tubes) {
 //            sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
 //            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
 //        }
 
-        sb.draw(mb4.getTexture(), mb4.getPosition().x, mb4.getPosition().y);
-        sb.draw(mb3.getTexture(), mb3.getPosition().x, mb3.getPosition().y);
+        sb.draw(cl.getTexture(), cl.getPosition().x, cl.getPosition().y);
+        sb.draw(bb.getTexture(), bb.getPosition().x, bb.getPosition().y);
+        sb.draw(sb1.getTexture(), sb1.getPosition().x, sb1.getPosition().y);
+        sb.draw(sb2.getTexture(), sb2.getPosition().x, sb2.getPosition().y);
+        sb.draw(mb1.getTexture(), mb1.getPosition().x, mb1.getPosition().y);
         sb.draw(mb2.getTexture(), mb2.getPosition().x, mb2.getPosition().y);
+        sb.draw(mb3.getTexture(), mb3.getPosition().x, mb3.getPosition().y);
         if(bgc == false){
-            sb.draw(mb1.getTexture(), mb1.getPosition().x, mb1.getPosition().y);
+            sb.draw(tr.getTexture(), tr.getPosition().x, tr.getPosition().y);
+        }
+        if(bgc == true){
+            sb.draw(trd.getTexture(), trd.getPosition().x, trd.getPosition().y);
         }
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         //sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
         //sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
-
+        sb.draw(bg, 0,0);
         sb.end();
         batch.begin();
         //desktop
 //        font.draw(batch, va, 100, 700);
 //        font.draw(batch, ti, 100, 670);
         //phone
-        font.draw(batch, va, 235, 1063);
-        bfont.draw(batch, ti, 85, 1024);
+//        font.draw(batch, va, 235, 1063);
+//        bfont.draw(batch, ti, 85, 1024);
+
+        //phone with two riders
+        font.draw(batch, va, 235, 1058);
+        bfont.draw(batch, ti, 85, 1018);
         batch.end();
     }
 
@@ -176,7 +222,12 @@ public class PlayState extends State {
         mb1.dispose();
         mb2.dispose();
         mb3.dispose();
-        mb4.dispose();
+        tr.dispose();
+        trd.dispose();
+        sb1.dispose();
+        sb2.dispose();
+        bb.dispose();
+        cl.dispose();
 
     }
 }
